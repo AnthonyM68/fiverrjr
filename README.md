f7bba37171ab135c5b7b3c00e98b307087f57c94
AnthonyM68 <anthony.montmirail@gmail.com>
2024-06-27 08:22:23 +0200

    Update database and README

---
+++ README.md
@@ -0,0 +1,81 @@
+# Bienvenue sur notre plateforme d'échange de services !
+## Sommaire
+1.	Introduction
+2.	Objectif du Projet
+3.	Fonctionnalités Principales
+4.	Technologies Utilisées
+5.	Organisation du Projet
+6.	Installation et Utilisation
+7.	Contributions
+8.	Contact
+
+## Introduction
+Bienvenue sur ma plateforme d'échange de services dédiée aux entrepreneurs et développeurs de la région Alsace (Haut-Rhin, Bas-Rhin). Mon application offre une expérience simplifiée et efficace pour la mise en relation de clients à la recherche de services spécifiques et de développeurs prêts à les offrir.
+
+## Objectif du Projet
+L'objectif principal de mon projet, réalisé dans le cadre de mon examen de fin de formation de développeur web, est de créer une plateforme intuitive et accessible, inspirée des meilleures pratiques d'UI/UX, tout en maintenant un design sobre et clair. Ma mission est de favoriser l'échange de compétences et de services au sein de notre communauté locale.
+
+## Fonctionnalités Principales
+•	Catégorisation avancée des services : Thèmes, Catégories, Sous-Catégories jusqu'au niveau de service spécifique.
+•	Inscription et Profils Utilisateur : Gestion complète des profils pour développeurs juniors et entrepreneurs.
+•	Interface d'Administration : Outil de gestion de contenu pour les administrateurs.
+•	Dispositif de Facturation : Système intégré pour la gestion des transactions entre utilisateurs.
+•	Accessibilité et Design Responsif : Couleurs simples et claires conformes aux normes d'accessibilité.
+
+## Technologies Utilisées
+•	Symfony PHP Framework
+•	Doctrine ORM
+•	Twig Template Engine
+•	Semantic/Semantic-thêmes/Jquery/Jquery-ui/Uikit/Bootstrap CSS Framework
+•	MySQL Database
+
+## Organisation du Projet
+
+## Le projet est structuré en plusieurs modules principaux :
+•	Backend : Gestion des données et logique métier.
+•	Frontend : Interface utilisateur basée sur des templates Twig.
+•	Administration : Interface d'administration sécurisée.
+## Installation et Utilisation
+
+## Pour installer et utiliser notre application, suivez les instructions suivantes :
+
+1.	Clonez le repository depuis GitHub.
+2.	Installez les dépendances avec Composer.
+3.	Configurez votre base de données MySQL.
+4.	Lancez le serveur Webpack.
+5.	Lancez le serveur Symfony.
+
+## Contributions
+S’agissant d’un projet d’examen la contribution n’est pas envisageable.
+## Contact
+Pour toute question ou commentaire, n'hésitez pas à nous contacter via notre page GitHub.
+
+# Schémas :
+
+> Réalisation des schémas conceptuels de données :
+> ![MCD](https://github.com/AnthonyM68/fiverrjr/blob/master/MCD.jpg)
+> ![UML](https://github.com/AnthonyM68/fiverrjr/blob/master/UML.jpg)
+> ![MLD](https://github.com/AnthonyM68/fiverrjr/blob/master/MLD.jpg)
+
+# NOTE :
+```php
+$ git clone https://github.com/AnthonyM68/fiverrjr.git
+```
+```php
+$ composer update 
+$ npm install
+```
+Utilisez la base de données fournie dans le dépot et modifiez le fichier .env si besoin
+
+> DATABASE_URL="mysql://root@127.0.0.1:3306/fiverrjr"
+> MAILER_DSN=smtp://localhost:1025
+fichier
+> .env
+
+```php
+
+<h3 align="center">Languages and Tools:</h3>
+<div align="center">
+<a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> </a>
+<a href="https://www.php.net" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="40" height="40"/> </a>
+</div>
--- fiverjr.sql
+++ fiverjr.sql
@@ -16,44 +16,75 @@
 
 
 -- Listage de la structure de la base pour fiverrjr
-CREATE DATABASE IF NOT EXISTS `fiverrjr` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `fiverrjr` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `fiverrjr`;
 
 -- Listage de la structure de table fiverrjr. category
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name_category` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `theme_id` int DEFAULT NULL,
+  `name_category` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `theme_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_64C19C159027487` (`theme_id`),
   CONSTRAINT `FK_64C19C159027487` FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table fiverrjr.category : ~1 rows (environ)
+-- Listage des données de la table fiverrjr.category : ~11 rows (environ)
 INSERT INTO `category` (`id`, `name_category`, `theme_id`) VALUES
-	(1, 'Sites Web personnalisés', 1);
+	(1, 'Création de Sites Web', 1),
+	(2, 'Sites E-commerce', 1),
+	(3, 'Développement Front-end', 1),
+	(4, 'Développement Back-end', 1),
+	(5, 'Développement Full-stack', 1),
+	(6, 'CMS et Frameworks', 1),
+	(7, 'API et Intégrations', 1),
+	(8, 'Applications iOS', 2),
+	(9, 'Applications Android', 2),
+	(10, 'Applications Hybrides', 2),
+	(11, 'Applications Android', 2);
 
 -- Listage de la structure de table fiverrjr. course
 CREATE TABLE IF NOT EXISTS `course` (
   `id` int NOT NULL AUTO_INCREMENT,
   `category_id` int DEFAULT NULL,
-  `name_course` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name_course` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_169E6FB912469DE2` (`category_id`),
   CONSTRAINT `FK_169E6FB912469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table fiverrjr.course : ~1 rows (environ)
+-- Listage des données de la table fiverrjr.course : ~19 rows (environ)
 INSERT INTO `course` (`id`, `category_id`, `name_course`) VALUES
-	(1, 1, 'Conception sur mesure'),
-	(2, 1, 'greh');
+	(1, 1, 'Développement de sites vitrines'),
+	(2, 1, 'Développement de blogs'),
+	(3, 1, 'Création de portfolios en ligne'),
+	(4, 2, 'Développement de boutiques en ligne avec Shopify'),
+	(5, 2, 'Développement de boutiques WooCommerce'),
+	(6, 2, 'Intégration de systèmes de paiement'),
+	(7, 3, 'HTML/CSS/JavaScript'),
+	(8, 3, 'Utilisation de frameworks front-end (React, Angular, Vue.js)'),
+	(9, 3, 'Optimisation des performances front-end'),
+	(10, 4, 'Développement avec Node.js'),
+	(11, 4, 'Développement avec Python/Django'),
+	(12, 4, 'Utilisation de PHP et frameworks (Laravel, Symfony)'),
+	(13, 5, 'Projets MERN (MongoDB, Express, React, Node.js)'),
+	(14, 5, 'Projets MEAN (MongoDB, Express, Angular, Node.js)'),
+	(15, 5, 'Projets LAMP (Linux, Apache, MySQL, PHP)'),
+	(16, 6, 'Développement de thèmes et plugins WordPress'),
+	(17, 6, 'Développement avec Joomla et Drupal'),
+	(18, 7, 'Développement d\'API RESTful'),
+	(19, 7, 'Intégration de services tiers (Stripe, PayPal, etc.)');
 
+-- Listage de la structure de table fiverrjr. doctrine_migration_versions
+IF NOT EXISTS ;
+
+-- Listage des données de la table fiverrjr.doctrine_migration_versions : ~1 rows (environ)
 -- Listage de la structure de table fiverrjr. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
@@ -83,69 +114,67 @@ CREATE TABLE IF NOT EXISTS `order` (
 -- Listage de la structure de table fiverrjr. service
 CREATE TABLE IF NOT EXISTS `service` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `orders_id` int DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `order_id` int DEFAULT NULL,
   `course_id` int NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` double NOT NULL,
   `duration` int NOT NULL,
   `create_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_E19D9AD2A76ED395` (`user_id`),
-  KEY `IDX_E19D9AD2CFFE9AD6` (`orders_id`),
-  CONSTRAINT `FK_E19D9AD2A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FK_E19D9AD2CFFE9AD6` FOREIGN KEY (`orders_id`) REFERENCES `order` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `IDX_E19D9AD2591CC992` (`course_id`),
+  KEY `IDX_E19D9AD28D9F6D38` (`order_id`),
+  CONSTRAINT `FK_E19D9AD2591CC992` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`),
+  CONSTRAINT `FK_E19D9AD28D9F6D38` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`),
+  CONSTRAINT `FK_E19D9AD2A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table fiverrjr.service : ~0 rows (environ)
-
--- Listage de la structure de table fiverrjr. service_course
-CREATE TABLE IF NOT EXISTS `service_course` (
-  `service_id` int NOT NULL,
-  `course_id` int NOT NULL,
-  PRIMARY KEY (`service_id`,`course_id`),
-  KEY `IDX_28596A8FED5CA9E6` (`service_id`),
-  KEY `IDX_28596A8F591CC992` (`course_id`),
-  CONSTRAINT `FK_28596A8F591CC992` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `FK_28596A8FED5CA9E6` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Listage des données de la table fiverrjr.service_course : ~0 rows (environ)
+-- Listage des données de la table fiverrjr.service : ~1 rows (environ)
+INSERT INTO `service` (`id`, `user_id`, `order_id`, `course_id`, `title`, `description`, `price`, `duration`, `create_date`) VALUES
+	(1, 1, NULL, 1, 'test', 'test', 10, 60, '2024-06-26 17:32:18');
 
 -- Listage de la structure de table fiverrjr. theme
 CREATE TABLE IF NOT EXISTS `theme` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name_theme` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name_theme` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table fiverrjr.theme : ~2 rows (environ)
+-- Listage des données de la table fiverrjr.theme : ~6 rows (environ)
 INSERT INTO `theme` (`id`, `name_theme`) VALUES
 	(1, 'Développement Web'),
-	(2, 'Développement Mobile');
+	(2, 'Développement Mobile'),
+	(3, 'Développement de Logiciels'),
+	(4, 'Bases de Données'),
+	(5, 'DevOps et Administration Système'),
+	(6, 'Intelligence Artificielle et Data Science');
 
 -- Listage de la structure de table fiverrjr. user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(180) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `roles` json NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `first_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phone_number` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone_number` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `date_register` datetime NOT NULL,
-  `picture` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `city` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `portfolio` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `bio` longtext COLLATE utf8mb4_unicode_ci,
+  `picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `portfolio` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bio` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `is_verified` tinyint(1) NOT NULL,
-  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table fiverrjr.user : ~0 rows (environ)
+-- Listage des données de la table fiverrjr.user : ~2 rows (environ)
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `first_name`, `last_name`, `phone_number`, `date_register`, `picture`, `city`, `portfolio`, `bio`, `is_verified`, `username`) VALUES
+	(1, 'admin@gmail.com', '[]', '$2y$13$030uvowwY4st0yG1THvBuuC5vjemI9k4kUMluwi.IBH32YCV.uVl2', NULL, NULL, NULL, '2024-06-20 06:48:38', NULL, NULL, NULL, NULL, 0, 'jad67tony'),
+	(2, 'user@gmail.com', '[]', '$2y$13$DSnyXKGD9ZXNx6CzHbDow.wm3d.HK4gjuHviyVNW/m2WK4.FcWlrO', NULL, NULL, NULL, '2024-06-27 05:50:34', NULL, NULL, NULL, NULL, 0, 'Anthony');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
