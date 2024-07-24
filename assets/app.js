fc13d03e6b1afa96dd5c8d176af951c43e99dad2
AnthonyM68 <anthony.montmirail@gmail.com>
2024-07-24 21:17:29 +0200

    footer update

--- assets/app.js
+++ assets/app.js
@@ -37,7 +37,12 @@ const LastUser = ({ role }) => {
     }
 
     if (!lastUser) {
-        return <div class="ui active loader"></div>;
+        return <div class="ui segment">
+        <div class="ui active inverted dimmer">
+          <div class="ui text loader">Loading</div>
+        </div>
+        <p></p>
+      </div>;
     }
 
     return <UserCard user={lastUser} />;
